99f55a3838e05e00e36f2f5cc8b4a43c178acf2c
zahraamustafa <zahraamu08@gmail.com>
2025-01-20 22:01:52 +0000

    updates 2

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 print("hello, welcome to zahraas food ordering chatbot")
 name = input("what is your name?")
 age = input("hello " + name + ", how old are you?")
+print("welcome " + name + ", this is this weeks menue.")
+def menu():
+    print("option 1")
+    print("option 2")
+    print("option 3")
+    print("option 4")
+    print("option 5")
+    print("option 6")
+    print("option 7")
+
+menu()
