bed0bf64d27c1526d6fe2c7918fbee0b598df9b8
zahraamustafa <zahraamu08@gmail.com>
2025-01-20 21:50:58 +0000

    updates

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-print("hello, welcome to zahraa's chatbot")
+print("hello, welcome to zahraas food ordering chatbot")
+name = input("what is your name?")
+age = input("hello " + name + ", how old are you?")
